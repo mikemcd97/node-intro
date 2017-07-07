1b355d64933408c04e4815d48d6e50a35bd4e2f3
Mike McDonald <mikemcd97@gmail.com>
2017-07-07 20:59:13 +0000

    first 2 answers

--- workshop.js
+++ workshop.js
@@ -6,17 +6,27 @@ function getDistance(pos1, pos2) {
 }
 
 function getIssPosition() {
-  return request(/* ... */)
+  return request('http://api.open-notify.org/iss-now.json')
   .then(
-    function(responce) {
-      // Parse as JSON
-      // Return object with lat and lng
+    function(response) {
+      var r = JSON.parse(response)
+      var lat = r.iss_position.latitude;
+      var lng= r.iss_position.longitude;
+      return r;
     }
   )
 }
 
 function getAddressPosition(address) {
-
+  var apiLink = ("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBrv6tJL-1KoDPGOEQ7r0LMWVa2UOOzfKg")
+  return request(apiLink)
+  .then(
+    function(response){
+      var r = JSON.parse(response);
+      var coordinates = r.results[0].geometry.location;
+      return coordinates;
+    }
+    )
 }
 
 function getCurrentTemperatureAtPosition(position) {
